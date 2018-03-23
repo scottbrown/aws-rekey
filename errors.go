46fd2279a9e45046836aa154d4dc11238874c13c
Scott Brown <scott.brown@unbounce.com>
2018-03-23 15:47:11 -0700

    Reformatting

--- errors.go
+++ errors.go
@@ -4,27 +4,33 @@ import (
 	"fmt"
 )
 
-type ConfigNotLoadedError struct{}
+type UnsupportedCredentialsProviderError struct{}
 
-type KeyTooYoungError struct {
-	key string
+func (e *UnsupportedCredentialsProviderError) Error() string {
+	return fmt.Sprintf("Could not find credentials in the usual places.  You may be storing your AWS access keys in environment variables or in a custom credentials provider, both of which are unsupported at this time.")
 }
 
-type MultipleKeysFoundError struct{}
-
-type NoKeysFoundError struct{}
+type ConfigNotLoadedError struct{}
 
 func (e *ConfigNotLoadedError) Error() string {
 	return fmt.Sprintf("Unable to load SDK config")
+}
+
+type KeyTooYoungError struct {
+	key string
 }
 
 func (e *KeyTooYoungError) Error() string {
 	return fmt.Sprintf("Key %s is less than 30 days old and there is no need for age-related rotation.  Use --force to override this.", e.key)
 }
 
+type MultipleKeysFoundError struct{}
+
 func (e *MultipleKeysFoundError) Error() string {
 	return fmt.Sprintf("You have multiple keys available.  Specify --key NUM to choose one")
 }
+
+type NoKeysFoundError struct{}
 
 func (e *NoKeysFoundError) Error() string {
 	return fmt.Sprintf("You do not have any access keys, or none can be found.")
